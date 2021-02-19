4b87219042b909b2c20d00ddc6c4caa87bb95ee7
Harsha Chenji <chenji@ohio.edu>
2021-02-19 17:10:06 +0000

    moved notebooks to desktop. fixed desktop folder error. added shortcut to 8888 on desktop

--- stage4/06-jupyter/00-run.sh
+++ stage4/06-jupyter/00-run.sh
@@ -3,15 +3,16 @@
 # install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 # install -m 644 one-time-pad.ipynb "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/one-time-pad.ipynb"
 
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Notebooks"
-cp files/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Notebooks/"
 
-# install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
-# install -v -o 1000 -g 1000 -m 644 "one-time-pad.ipynb" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Notebooks"
+cp files/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Notebooks/"
 
-# install -v -o 1000 -g 1000 -m 644 "ohio.jpg" "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
+install -v -o 1000 -g 1000 -m 755 "Jupyter.desktop" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/Jupyter.desktop"
 
-cp ohio.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
+install -v -o 1000 -g 1000 -m 644 "ohio.jpg" "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
+
+# cp ohio.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
 
 if [[ -f "${ROOTFS_DIR}/etc/xdg/autostart/piwiz.desktop" ]]; then
 	rm "${ROOTFS_DIR}/etc/xdg/autostart/piwiz.desktop"
@@ -25,3 +26,4 @@ pip3 install --upgrade matplotlib
 pip3 install --upgrade pillow
 pip3 install --upgrade blinkt
 EOF
+
